06553ce81342d3845a72c86fb38fd1fa50839bd8
Christian Winter <Christian.Winter81@me.com>
2017-05-27 12:29:15 +0200

    Added meaningful documentation comments

--- Source/src/PCx.IO.Compression/PCx/IO/Compression/DeflateParallelStream.cs
+++ Source/src/PCx.IO.Compression/PCx/IO/Compression/DeflateParallelStream.cs
@@ -5,7 +5,13 @@ using System.IO.Compression;
 namespace PCx.IO.Compression
 {
 	/// <summary>
-	/// Deflate parallel stream.
+	/// Provides methods and properties for compressing and decompressing streams by using the Deflate algorithm.
+	/// In comparison to <see cref="DeflateStream"/> this stream is optimized by using parallel algorithms.
+	/// <remarks>
+	/// A compressed stream by <see cref="DeflateParallelStream"/> is incompatible with decompression by <see cref="DeflateStream"/>.
+	/// A compressed stream by <see cref="DeflateStream"/> is incompatible with decompression by <see cref="DeflateParallelStream"/>.
+	/// A with <see cref="DeflateParallelStream"/> compressed stream has to be decompressed with <see cref="DeflateParallelStream"/>.
+	/// </remarks>
 	/// </summary>
 	public sealed class DeflateParallelStream : Stream
 	{
@@ -22,10 +28,11 @@ namespace PCx.IO.Compression
 		#region Constructor
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="T:PCx.IO.Compression.DeflateParallelStream"/> class.
+		/// Initializes a new instance of the <see cref="DeflateParallelStream"/> class
+		/// using the specified <paramref name="stream"/> and <paramref name="compressionMode"/>.
 		/// </summary>
-		/// <param name="stream">Stream.</param>
-		/// <param name="compressionMode">Compression mode.</param>
+		/// <param name="stream">The stream to compress or decompress.</param>
+		/// <param name="compressionMode">One of the enumeration values that indicates whether to compress or decompress the stream.</param>
 		public DeflateParallelStream(Stream stream, CompressionMode compressionMode)
 		{
 			_Stream = stream;
@@ -51,11 +58,16 @@ namespace PCx.IO.Compression
 		}
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="T:PCx.IO.Compression.DeflateParallelStream"/> class.
+		/// Initializes a new instance of the <see cref="DeflateParallelStream"/> class
+		/// using the specified <paramref name="stream"/>, <paramref name="compressionLevel"/> and <paramref name="bufferSize"/>.
 		/// </summary>
-		/// <param name="stream">Stream.</param>
-		/// <param name="compressionLevel">Compression level.</param>
-		/// <param name="bufferSize">Buffer size.</param>
+		/// <param name="stream">The stream to compress.</param>
+		/// <param name="compressionLevel">One of the enumeration values that indicates whether to emphasize speed or compression efficiency when compressing the stream.</param>
+		/// <param name="bufferSize">The size of the buffer. This value must be greater than zero.</param>
+		/// <remarks>
+		/// The specified <paramref name="bufferSize"/> affects the parallelization potential and
+		/// and should be selected in dependency of the size of the <paramref name="stream"/> stream.
+		/// </remarks>
 		public DeflateParallelStream(Stream stream, CompressionLevel compressionLevel, int bufferSize)
 		{
 			_Stream = stream;
@@ -68,9 +80,9 @@ namespace PCx.IO.Compression
 		#region Properties
 
 		/// <summary>
-		/// Gets the base stream.
+		/// Gets a reference to the underlying stream.
 		/// </summary>
-		/// <value>The base stream.</value>
+		/// <value>A stream object that represents the underlying stream.</value>
 		public Stream BaseStream
 		{
 			get
@@ -86,9 +98,8 @@ namespace PCx.IO.Compression
 		#region Properties
 
 		/// <summary>
-		/// Gets a value indicating whether this <see cref="T:PCx.IO.Compression.DeflateParallelStream"/> can read.
+		/// See <see cref="Stream.CanRead"/> 
 		/// </summary>
-		/// <value><c>true</c> if can read; otherwise, <c>false</c>.</value>
 		public override bool CanRead
 		{
 			get
@@ -98,9 +109,8 @@ namespace PCx.IO.Compression
 		}
 
 		/// <summary>
-		/// Gets a value indicating whether this <see cref="T:PCx.IO.Compression.DeflateParallelStream"/> can write.
+		/// See <see cref="Stream.CanWrite"/> 
 		/// </summary>
-		/// <value><c>true</c> if can write; otherwise, <c>false</c>.</value>
 		public override bool CanWrite
 		{
 			get
@@ -110,9 +120,8 @@ namespace PCx.IO.Compression
 		}
 
 		/// <summary>
-		/// Gets a value indicating whether this <see cref="T:PCx.IO.Compression.DeflateParallelStream"/> can seek.
+		/// See <see cref="Stream.CanSeek"/> 
 		/// </summary>
-		/// <value><c>true</c> if can seek; otherwise, <c>false</c>.</value>
 		public override bool CanSeek
 		{
 			get
@@ -122,9 +131,8 @@ namespace PCx.IO.Compression
 		}
 
 		/// <summary>
-		/// Gets the length.
+		/// See <see cref="Stream.Length"/> 
 		/// </summary>
-		/// <value>The length.</value>
 		public override long Length
 		{
 			get
@@ -134,9 +142,8 @@ namespace PCx.IO.Compression
 		}
 
 		/// <summary>
-		/// Gets or sets the position.
+		/// See <see cref="Stream.Position"/> 
 		/// </summary>
-		/// <value>The position.</value>
 		public override long Position
 		{
 			get
@@ -154,31 +161,23 @@ namespace PCx.IO.Compression
 		#region Methods
 
 		/// <summary>
-		/// Read the specified buffer, offset and count.
+		/// See <see cref="Stream.Read"/> 
 		/// </summary>
-		/// <returns>The read.</returns>
-		/// <param name="buffer">Buffer.</param>
-		/// <param name="offset">Offset.</param>
-		/// <param name="count">Count.</param>
 		public override int Read(byte[] buffer, int offset, int count)
 		{
 			return _DecompressStream.Read(buffer, offset, count);
 		}
 
 		/// <summary>
-		/// Write the specified buffer, offset and count.
+		/// See <see cref="Stream.Write"/> 
 		/// </summary>
-		/// <returns>The write.</returns>
-		/// <param name="buffer">Buffer.</param>
-		/// <param name="offset">Offset.</param>
-		/// <param name="count">Count.</param>
 		public override void Write(byte[] buffer, int offset, int count)
 		{
 			_CompressStream.Write(buffer, offset, count);
 		}
 
 		/// <summary>
-		/// Flush this instance.
+		/// See <see cref="Stream.Flush"/> 
 		/// </summary>
 		public override void Flush()
 		{
@@ -189,30 +188,24 @@ namespace PCx.IO.Compression
 		}
 
 		/// <summary>
-		/// Seek the specified offset and origin.
+		/// See <see cref="Stream.Seek"/> 
 		/// </summary>
-		/// <returns>The seek.</returns>
-		/// <param name="offset">Offset.</param>
-		/// <param name="origin">Origin.</param>
 		public override long Seek(long offset, SeekOrigin origin)
 		{
 			throw new NotSupportedException();
 		}
 
 		/// <summary>
-		/// Sets the length.
+		/// See <see cref="Stream.SetLength"/> 
 		/// </summary>
-		/// <param name="value">Value.</param>
 		public override void SetLength(long value)
 		{
 			throw new NotSupportedException();
 		}
 
 		/// <summary>
-		/// Dispose the specified disposing.
+		/// See <see cref="Stream.Dispose(bool)"/> 
 		/// </summary>
-		/// <returns>The dispose.</returns>
-		/// <param name="disposing">If set to <c>true</c> disposing.</param>
 		protected override void Dispose(bool disposing)
 		{
 			try
