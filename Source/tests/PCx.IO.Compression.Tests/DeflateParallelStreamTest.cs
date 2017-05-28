ab6085cd97364e8715b19284d60449345f939dbb
Christian Winter <Christian.Winter81@me.com>
2017-05-28 14:35:32 +0200

    Added de-/compress validation tests

--- Source/tests/PCx.IO.Compression.Tests/DeflateParallelStreamTest.cs
+++ Source/tests/PCx.IO.Compression.Tests/DeflateParallelStreamTest.cs
@@ -21,6 +21,7 @@
 // THE SOFTWARE.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -33,88 +34,23 @@ namespace PCx.IO.Compression.Tests
 {
 	public static class DeflateParallelStreamTest
 	{
+		#region Fields
+
+		private static IReadOnlyList<byte[]> DataList = new List<byte[]>
+		{
+			GenerateData(1024, 30),
+			GenerateData(1024, 1120),
+			GenerateData(1024, 250),
+			GenerateData(1024, 333)
+		};
+
+		private static IEnumerable<byte> DataListFlat => DataList.SelectMany(data => data);
+
+		private static int DataListSize => DataList.Sum(data => data.Length);
+
+		#endregion
+
 		#region Tests
-
-		[Fact]
-		public static void DeflateParallelStream_Validation()
-		{
-			var dataList = new[]
-			{
-				GenerateData(1024, 64),
-				GenerateData(1024, 128),
-				GenerateData(1024, 1120)
-			};
-
-			byte[] compressedData;
-			byte[] decompressedData;
-
-			using (var compressed = new MemoryStream())
-			{
-				using (var compressStream = new DeflateParallelStream(compressed, CompressionLevel.Optimal, 128 * 1024, leaveOpen: true))
-				{
-					foreach (var data in dataList)
-					{
-						compressStream.Write(data, 0, data.Length);
-					}
-				}
-
-				compressedData = compressed.ToArray();
-
-				compressed.Seek(0, SeekOrigin.Begin);
-
-				using (var decompressStream = new DeflateParallelStream(compressed, CompressionMode.Decompress, leaveOpen: true))
-				{
-					using (var decompressed = new MemoryStream())
-					{
-						decompressStream.CopyTo(decompressed);
-
-						decompressedData = decompressed.ToArray();
-					}
-				}
-			}
-
-			Assert.True(compressedData.Length < dataList.Sum(data => data.Length));
-
-			Assert.True(dataList.SelectMany(data => data).SequenceEqual(decompressedData));
-		}
-
-		[Fact]
-		public static void DeflateParallelStream_CompressMode_NoOperation_StreamUntouched()
-		{
-			using (var stream = new MemoryStream())
-			{
-				using (var compressStream = new DeflateParallelStream(stream, CompressionMode.Compress, leaveOpen: true))
-				{
-					// No Operation
-				}
-
-				Assert.Equal(0, stream.Position);
-				Assert.Equal(0, stream.Length);
-			}
-		}
-
-		[Fact]
-		public static void DeflateParallelStream_DecompressMode_NoOperation_StreamUntouched()
-		{
-			using (var stream = new MemoryStream())
-			{
-				using (var compressStream = new DeflateParallelStream(stream, CompressionMode.Compress, leaveOpen: true))
-				{
-					var data = GenerateData(1024, 1024);
-
-					compressStream.Write(data, 0, data.Length);
-				}
-
-				stream.Seek(0, SeekOrigin.Begin);
-
-				using (var decompressSTream = new DeflateParallelStream(stream, CompressionMode.Decompress, leaveOpen: true))
-				{
-					// No Operation
-				}
-
-				Assert.Equal(0, stream.Position);
-			}
-		}
 
 		[Fact]
 		public static void DeflateParallelStream_Constructor_ArgumentValidation()
@@ -129,8 +65,71 @@ namespace PCx.IO.Compression.Tests
 			Assert.Throws<ArgumentOutOfRangeException>(() => new DeflateParallelStream(Stream.Null, CompressionLevel.NoCompression, bufferSize: -1, leaveOpen: true));
 		}
 
+		[Theory]
+		[InlineData(CompressionLevel.Optimal)]
+		[InlineData(CompressionLevel.Fastest)]
+		public static void DeflateParallelStream_Compress_SizeDecreases(CompressionLevel compressionLevel)
+		{
+			byte[] compressedData;
+
+			using (var compressed = new MemoryStream())
+			{
+				using (var compressStream = new DeflateParallelStream(compressed, compressionLevel, 32 * 1024, leaveOpen: true))
+				{
+					foreach (var data in DataList)
+					{
+						compressStream.Write(data, 0, data.Length);
+					}
+				}
+
+				compressedData = compressed.ToArray();
+			}
+
+			Console.WriteLine($"Source size = {DataListSize} -> Compressed size = {compressedData.Length} ({compressionLevel})");
+
+			Assert.True(compressedData.Length < DataListSize);
+		}
+
 		[Fact]
-		public static void DeflateParallelStream_CompressMode_ArgumentValidation()
+		public static void DeflateParallelStream_Compress_SizeIncreases()
+		{
+			byte[] compressedData;
+
+			using (var compressed = new MemoryStream())
+			{
+				using (var compressStream = new DeflateParallelStream(compressed, CompressionLevel.NoCompression, 32 * 1024, leaveOpen: true))
+				{
+					foreach (var data in DataList)
+					{
+						compressStream.Write(data, 0, data.Length);
+					}
+				}
+
+				compressedData = compressed.ToArray();
+			}
+
+			Console.WriteLine($"Source size = {DataListSize} -> Compressed size = {compressedData.Length} (NoCompression)");
+
+			Assert.True(compressedData.Length > DataListSize);
+		}
+
+		[Fact]
+		public static void DeflateParallelStream_Compress_NoOperation_StreamUntouched()
+		{
+			using (var stream = new MemoryStream())
+			{
+				using (var compressStream = new DeflateParallelStream(stream, CompressionMode.Compress, leaveOpen: true))
+				{
+					// No Operation
+				}
+
+				Assert.Equal(0, stream.Position);
+				Assert.Equal(0, stream.Length);
+			}
+		}
+
+		[Fact]
+		public static void DeflateParallelStream_Compress_ArgumentValidation()
 		{
 			DeflateParallelStream stream;
 
@@ -155,8 +154,68 @@ namespace PCx.IO.Compression.Tests
 			Assert.Throws<ObjectDisposedException>(() => stream.Flush());
 		}
 
+		[Theory]
+		[InlineData(CompressionLevel.Optimal)]
+		[InlineData(CompressionLevel.Fastest)]
+		[InlineData(CompressionLevel.NoCompression)]
+		public static void DeflateParallelStream_Decompress_EqualsSourceData(CompressionLevel compressionLevel)
+		{
+			byte[] compressedData;
+			byte[] decompressedData;
+
+			using (var compressed = new MemoryStream())
+			{
+				using (var compressStream = new DeflateParallelStream(compressed, compressionLevel, 32 * 1024, leaveOpen: true))
+				{
+					foreach (var data in DataList)
+					{
+						compressStream.Write(data, 0, data.Length);
+					}
+				}
+
+				compressedData = compressed.ToArray();
+
+				compressed.Seek(0, SeekOrigin.Begin);
+
+				using (var decompressStream = new DeflateParallelStream(compressed, CompressionMode.Decompress, leaveOpen: true))
+				{
+					using (var decompressed = new MemoryStream())
+					{
+						decompressStream.CopyTo(decompressed, bufferSize: 32 * 1024);
+
+						decompressedData = decompressed.ToArray();
+					}
+				}
+			}
+
+			Assert.True(DataListFlat.SequenceEqual(decompressedData));
+		}
+
 		[Fact]
-		public static void DeflateParallelStream_DecompressMode_ArgumentValidation()
+		public static void DeflateParallelStream_Decompress_NoOperation_StreamUntouched()
+		{
+			using (var stream = new MemoryStream())
+			{
+				using (var compressStream = new DeflateParallelStream(stream, CompressionMode.Compress, leaveOpen: true))
+				{
+					var data = GenerateData(1024, 1024);
+
+					compressStream.Write(data, 0, data.Length);
+				}
+
+				stream.Seek(0, SeekOrigin.Begin);
+
+				using (var decompressSTream = new DeflateParallelStream(stream, CompressionMode.Decompress, leaveOpen: true))
+				{
+					// No Operation
+				}
+
+				Assert.Equal(0, stream.Position);
+			}
+		}
+
+		[Fact]
+		public static void DeflateParallelStream_Decompress_ArgumentValidation()
 		{
 			DeflateParallelStream stream;
 
